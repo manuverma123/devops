26b8eb505ade581fb2fa2add567a0415b0d99ef5
manuverma123 <133035155+manuverma123@users.noreply.github.com>
2024-04-13 14:57:54 +0530

    Update DevopsIntegrationApplication.java

--- devops-automation-main/src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ devops-automation-main/src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DevopsIntegrationApplication {
 
-	@GetMapping
+	@GetMapping("/welcome")
 	public String message(){
 		return "welcome to javatechie";
 	}
